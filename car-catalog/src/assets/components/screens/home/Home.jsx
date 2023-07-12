69cc7766053f2b8532eb9e1ab4e2a29c80bb706d
kira <k-korni1ov@yandex.ru>
2023-07-12 16:40:27 +0400

    feat: done adding info from cars

--- car-catalog/src/assets/components/screens/home/Home.jsx
+++ car-catalog/src/assets/components/screens/home/Home.jsx
@@ -1,7 +1,32 @@
 import styles from './Home.module.css'
+import {cars} from './cars.data.js'
+
 
 const Home=()=> {
   
+
+    //  const cars = [
+    //     { id: 1,
+    //      name: 'Toyota Camry',
+    //      price:24970,
+    //      image: 'car1.jpg',
+     
+    //      },
+    //      { id: 2,
+    //          name: 'Ferrari',
+    //          price:54970,
+    //          image: 'car2.jpg',
+         
+    //          }
+    //          ,
+    //      { id: 3,
+    //          name: 'BMW',
+    //          price:94970,
+    //          image: 'car3.jpg',
+         
+    //          }
+    //  ]
+
     return (
       
        <div>
@@ -9,19 +34,27 @@ const Home=()=> {
           Car catalog
         </h1>
             <div>
-                <div  className={styles.item}>
+                {cars.map(car=>(
+
+                    <div key={car.id} className={styles.item}>
                     <div   className={styles.image}
                     style={{
-                        backgroundImage: 'url(/car1.jpg)',
+                        backgroundImage: `url(${car.image})`,
                     }}
                     />
                     <div className={styles.info}>
-                    <h2>Car1</h2>
-                    <p>Car 1 desc</p>
-                    <p>10000$</p>
+                    <h2>{car.name}</h2>
+                    <p>{new Intl.NumberFormat('en-US',{
+                        style: 'currency',
+                        currency: "USD",
+                        currencyDisplay: 'narrowSymbol',
+                    }).format(car.price)}</p>
                     <button>Read mode</button>
                     </div>
-                </div>
+                    </div>
+
+                ))}
+               
             </div>
        </div>
       
---
+++ car-catalog/src/assets/components/screens/home/cars.data.js
@@ -0,0 +1,21 @@
+export const cars = [
+   { id: 1,
+    name: 'Toyota Camry',
+    price:24970,
+    image: 'car1.jpg',
+
+    },
+    { id: 2,
+        name: 'Ferrari',
+        price:54970,
+        image: 'car2.jpg',
+    
+        }
+        ,
+    { id: 3,
+        name: 'BMW',
+        price:94970,
+        image: 'car3.jpg',
+    
+        }
+]
